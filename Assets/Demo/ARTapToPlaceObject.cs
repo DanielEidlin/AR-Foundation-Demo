44bcf7c96f8806a4c8edb9c3f0dee1f59ab15817
Daniel Eidlin <daniel.eidlin@gmail.com>
2021-03-24 21:35:30 +0200

    Init commit

---
+++ Assets/Demo/ARTapToPlaceObject.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+using System;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARTapToPlaceObject : MonoBehaviour
+{
+    public GameObject objectToPlace;
+    public GameObject placementIndicator;
+    private ARSessionOrigin arOrigin;
+    private ARRaycastManager raycastManager;
+    private Pose placementPose;
+    private bool isPlacementPoseValid = false;
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        raycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        isPlacementPoseValid = hits.Count > 0;
+        if (isPlacementPoseValid)
+        {
+            placementPose = hits[0].pose;
+            var cameraForward = Camera.main.transform.forward;
+            var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+        }
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (isPlacementPoseValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void PlaceObject(){
+        Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+        raycastManager = FindObjectOfType<ARRaycastManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+
+        if (isPlacementPoseValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            PlaceObject();
+        }
+    }
+
+}
